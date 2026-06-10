2ae3c8ecd0d504a3a0b94386229bc5804cdc208c
BobSanDiego <bob.reap@gmail.com>
2026-06-10 02:29:12 -0700

    Add initial Community Framework Manager schema installer

---
+++ public/wp-content/plugins/community-framework-manager/community-framework-manager.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Community Framework Manager
+ * Description: Reusable framework/axis/term manager for community profile taxonomies and recommendations.
+ * Version: 0.1.0
+ * Author: Teachers.Net
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+define('CFM_VERSION', '0.1.0');
+define('CFM_PLUGIN_FILE', __FILE__);
+define('CFM_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('CFM_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once CFM_PLUGIN_DIR . 'includes/class-cfm-schema.php';
+require_once CFM_PLUGIN_DIR . 'includes/class-cfm-activator.php';
+
+register_activation_hook(__FILE__, ['CFM_Activator', 'activate']);
